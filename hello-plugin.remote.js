5cde59c786b722a75dc16a4947799773d0c28a25
Vinicius <suburbanizar@gmail.com>
2026-09-07 02:51:48 -0300

    Update hello-plugin.remote.js

--- hello-plugin.remote.js
+++ hello-plugin.remote.js
@@ -1,34 +1,81 @@
-export default {
-  manifest: {
-    id: "hello-remote",
-    name: "Hello Remote",
-    version: "1.0.0",
-    description: "Plugin de teste",
-    author: "IITCX"
-  },
+// hello-iitcx.user.js
+(function () {
+  "use strict";
 
-  setup(ctx) {
-    ctx.addPanel({
-      id: "hello-panel",
-      title: "Hello Remote",
+  const plugin = {
+    manifest: {
+      id: "hello-iitcx",
+      name: "Hello IITCX",
+      version: "1.0.0",
+      description: "Plugin nativo de teste do IITCX",
+      author: "Suburbanno"
+    },
 
-      render(container) {
-        container.textContent = "Plugin instalado com sucesso";
+    setup(ctx) {
+      let selectedPortals = 0;
 
-        return () => {
-          container.replaceChildren();
-        };
-      }
-    });
+      ctx.on("portalSelected", function () {
+        selectedPortals += 1;
 
-    ctx.addButton({
-      id: "hello-button",
-      title: "Testar plugin",
-      icon: "Hi",
+        const counter = document.querySelector(
+          "[data-hello-iitcx-counter]"
+        );
 
-      onClick() {
-        ctx.log("Botão do plugin clicado");
-      }
-    });
+        if (counter) {
+          counter.textContent =
+            "Portais selecionados: " + selectedPortals;
+        }
+      });
+
+      ctx.addPanel({
+        id: "hello-panel",
+        title: "Hello IITCX",
+
+        render(container) {
+          container.innerHTML = `
+            <div data-hello-iitcx-counter>
+              Portais selecionados: 0
+            </div>
+            <p>Plugin nativo carregado.</p>
+          `;
+
+          return function cleanup() {
+            container.replaceChildren();
+          };
+        }
+      });
+
+      ctx.addButton({
+        id: "hello-button",
+        title: "Testar Hello IITCX",
+        icon: "Hi",
+
+        onClick() {
+          ctx.log(
+            "Portais selecionados: " + selectedPortals
+          );
+        }
+      });
+
+      return function cleanup() {
+        ctx.log("Hello IITCX removido");
+      };
+    }
+  };
+
+  /*
+   * O plugin é registrado pela API pública do IITCX.
+   */
+  if (
+    window.iitcx &&
+    window.iitcx.plugins &&
+    typeof window.iitcx.plugins.register === "function"
+  ) {
+    window.iitcx.plugins.register(plugin);
+    return;
   }
-};
+
+  console.error(
+    "[Hello IITCX] A API window.iitcx.plugins não está disponível."
+  );
+})();
